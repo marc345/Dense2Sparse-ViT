097521074f14dfe3bdb690c082ccc455da6e1193
Marc Seger <m.seger345@gmail.com>
2021-09-03 08:47:38 +0200

    reformatting of main script

--- main.py
+++ main.py
@@ -45,32 +45,6 @@ class_names = image_datasets['train'].classes
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-######################################################################
-# Visualize a few images
-# ^^^^^^^^^^^^^^^^^^^^^^
-# Let's visualize a few training images so as to understand the data
-# augmentations.
-
-def imshow(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
-
-
-# Get a batch of training data
-#inputs, classes = next(iter(dataloaders['train']))
-
-# Make a grid from batch
-#out = torchvision.utils.make_grid(inputs)
-
-#imshow(out, title=[class_names[x] for x in classes])
 
 def get_model(args, pretrained=True):
     model_names = sorted(name for name in models.__dict__
@@ -101,16 +75,6 @@ def get_model(args, pretrained=True):
 
 ######################################################################
 # Training the model
-# ------------------
-#
-# Now, let's write a general function to train a model. Here, we will
-# illustrate:
-#
-# -  Scheduling the learning rate
-# -  Saving the best model
-#
-# In the following, parameter ``scheduler`` is an LR scheduler object from
-# ``torch.optim.lr_scheduler``.
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
@@ -182,45 +146,10 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
-######################################################################
-# Visualizing the model predictions
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#
-# Generic function to display predictions for a few images
-#
-
-def visualize_model(model, num_images=6):
-    was_training = model.training
-    model.eval()
-    images_so_far = 0
-    fig = plt.figure()
-
-    with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders['val']):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-
-            for j in range(inputs.size()[0]):
-                images_so_far += 1
-                ax = plt.subplot(num_images//2, 2, images_so_far)
-                ax.axis('off')
-                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-                imshow(inputs.cpu().data[j])
-
-                if images_so_far == num_images:
-                    model.train(mode=was_training)
-                    return
-        model.train(mode=was_training)
 
 ######################################################################
-# Finetuning the convnet
-# ----------------------
-#
-# Load a pretrained model and reset final fully connected layer.
-#
+# Finetuning
+
 
 args = {
     'model_name': 'dino_small',
@@ -251,21 +180,8 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 ######################################################################
 # Train and evaluate
-# ^^^^^^^^^^^^^^^^^^
-#
-# It should take around 15-25 min on CPU. On GPU though, it takes less than a
-# minute.
-#
+
 
 model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=25)
-
-######################################################################
-#
-
-#visualize_model(model)
-
-
-#plt.show()
-#plt.ioff()
 
