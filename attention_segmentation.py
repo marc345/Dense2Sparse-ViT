19eceaf0bfefe20c6a26b38fc2f53d8122cd59c7
Marc Seger <m.seger345@gmail.com>
2021-09-28 15:37:24 +0200

    updated helper functions the display patch selection on selected held out test images

--- attention_segmentation.py
+++ attention_segmentation.py
@@ -25,19 +25,19 @@ def get_attention_masks(args, images, model):
     if args.is_dist:
         if args.use_shape:
             attentions = attentions[:, :, 1, 2:]  # shape: (B, H, 1, N-2), keep only self-attention for shape query
-            attentions = attentions.reshape(nh, -1)  # shape: (B, H, N-2), use distillation token attention
+            #attentions = attentions.reshape(nh, -1)  # shape: (B, H, N-2), use distillation token attention
         else:
             attentions = attentions[:, :, 0, 2:]  # shape: (B, H, 1, N-2), keep only self-attention for CLS query
-            attentions = attentions.reshape(nh, -1)  # shape: (B, H, N-2), use class token attention
+            #attentions = attentions.reshape(nh, -1)  # shape: (B, H, N-2), use class token attention
     else:
-        attentions = attentions[:, :, 0, 1:].reshape(nh, -1)  # shape: (B, H, N-1), use class token attention
+        attentions = attentions[:, :, 0, 1:]#.reshape(nh, -1)  # shape: (B, H, N-1), use class token attention
 
     # we keep only a certain percentage of the mass
     # torch.sort(): A namedtuple of (values, indices) is returned, where the values are the sorted values and indices
     # are the indices of the elements in the original input tensor.
-    val, idx = torch.sort(attentions)  # sort attention weights in ascending order
-    val /= torch.sum(val, dim=1, keepdim=True)  # divide by the sum along token dimension
-    cum_val = torch.cumsum(val, dim=1)  # cumulative sum of all previous sub-tensors along the specified dimension
+    val, idx = torch.sort(attentions, dim=-1)  # sort attention weights in ascending order
+    val /= torch.sum(val, dim=-1, keepdim=True)  # divide by the sum along token dimension
+    cum_val = torch.cumsum(val, dim=-1)  # cumulative sum of all previous sub-tensors along the specified dimension
     th_attn = cum_val > (1 - args.threshold)  # boolean tensor of shape (H, N-#spezialized_tokens)
     th_attn = torch.zeros_like(th_attn).scatter_(dim=-1, index=idx, src=th_attn)
 
@@ -71,13 +71,13 @@ def generate_patch_mask(image_height, keep_decisions):
 
     return patch_mask
 
-def display_patch_drop(images, keep_decisions, save_path, args, epoch_num, classifications, final_cls_attn=None,
+def display_patch_drop(images, keep_decisions, args, epoch_num, classifications, final_cls_attn=None,
                        patch_indices=None, th_attn_mask=None, display_segmentation=False, max_heads=True, alpha=0.5):
     B, _, H, W = images.shape
 
     if epoch_num == 0:
         raw_images = images.clone()
-        save_image_grid(int(max(1, B / 4)), 4, save_path, raw_images.permute(0, 2, 3, 1).cpu().numpy(), epoch_num,
+        save_image_grid(int(max(1, B / 4)), 4, raw_images.permute(0, 2, 3, 1).cpu().numpy(), args, epoch_num,
                         classifications=classifications, save_raw=True)
 
     images = images.permute(0, 2, 3, 1).cpu().numpy()
@@ -96,16 +96,17 @@ def display_patch_drop(images, keep_decisions, save_path, args, epoch_num, class
 
     masked_images = (images * 255).astype(np.int32).copy()
 
-    save_image_grid(int(max(1, B / 4)), 4, save_path, masked_images, epoch_num, final_cls_attn=final_cls_attn_mask,
+    save_image_grid(int(max(1, B / 4)), 4, masked_images, args, epoch_num, final_cls_attn=final_cls_attn_mask
+                    if final_cls_attn is not None else None,
                     classifications=classifications)
 
     if display_segmentation:
-        if max_heads:
-            # use maximum attention value across final attention heads
-            th_attn_mask, _ = torch.max(th_attn_mask, dim=1)
-        else:
-            # use mean attention value across final attention heads
-            th_attn_mask = torch.mean(th_attn_mask, dim=1)
+        #if max_heads:
+        #    # use maximum attention value across final attention heads
+        #    th_attn_mask, _ = torch.max(th_attn_mask, dim=1)
+        #else:
+        #    # use mean attention value across final attention heads
+        #    th_attn_mask = torch.mean(th_attn_mask, dim=1)
 
         th_attn_mask = th_attn_mask.cpu().numpy()
 
@@ -126,14 +127,14 @@ def display_patch_drop(images, keep_decisions, save_path, args, epoch_num, class
 
         jaccard_sim = get_jaccard_similarity(patch_drop_mask.cpu().numpy(), th_attn_mask[0])
 
-        save_image_grid(int(max(1, B/4)), 4, save_path, masked_images, epoch_num, final_cls_attn=final_cls_attn,
+        save_image_grid(int(max(1, B/4)), 4, masked_images, args, epoch_num, final_cls_attn=final_cls_attn,
                         classifications=classifications, suptitles=jaccard_sim,
                         display_segmentation=display_segmentation, contours=contours)
 
-def save_image_grid(num_rows, num_cols, save_path, images, epoch_num, final_cls_attn=None, classifications=None,
+def save_image_grid(num_rows, num_cols, images, args, epoch_num, final_cls_attn=None, classifications=None,
                     suptitles=None, display_segmentation=False,
                     save_raw=False, contours=None):
-
+    save_path = args.save_path + args.job_name + '/'
     pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
 
     fig, axs = plt.subplots(num_rows, num_cols, figsize=(30, 30))
@@ -156,41 +157,40 @@ def save_image_grid(num_rows, num_cols, save_path, images, epoch_num, final_cls_
             axs[row, col].set_title(title_str, fontsize=34)
             axs[row, col].imshow(images[(row * num_cols) + col], interpolation='nearest')
             if final_cls_attn is not None:
-                axs[row, col].imshow(final_cls_attn[(row * num_cols) + col].squeeze(0),
-                                     cmap='hot', alpha=0.1, interpolation='nearest')
+                # heatmap = axs[row, col].imshow(final_cls_attn[(row * num_cols) + col].squeeze(0), cmap='hot', alpha=1,
+                #                               interpolation='nearest')
+                heatmap = axs[row, col].pcolormesh(final_cls_attn[(row * num_cols) + col].squeeze(0), cmap='hot',
+                                                   alpha=0.4)
 
-    fig.suptitle(f'Epoch {epoch_num}\n'
-                 f'{"With Jaccard index between kept patches and attention segmentation mask" if display_segmentation else ""}',
-                 fontsize=48)
-    #fig.tight_layout()
-    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.1, hspace=0.1)
+    if final_cls_attn is not None:
+        cbar_ax = fig.add_axes([0.05, 0.05, 0.9, 0.03])
+        cbar = fig.colorbar(heatmap, cax=cbar_ax, orientation='horizontal')
+        cbar.ax.set_xlabel('Final CLS attention weight amount (max over all heads)', fontdict={'size': 48})
+        cbar.ax.tick_params(labelsize=36)
+
+    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.87, top=0.9, wspace=0.1, hspace=0.1)
 
     if display_segmentation:
-        fig.suptitle(f'Epoch {epoch_num}\n'
+        fig.suptitle(f'Epoch {epoch_num+1}\n'
                      f'{"With Jaccard index between kept patches and attention segmentation mask" if display_segmentation else ""}', fontsize=48)
-        plt.savefig(f'{save_path}/images_patch_drop_and_attn_segmentation_{epoch_num}_epoch.jpg')
+        plt.savefig(f'{save_path}/images_patch_drop_and_attn_segmentation_{epoch_num+1}_epoch.jpg')
     elif save_raw:
         fig.suptitle('Unmodified Images', fontsize=48)
         plt.savefig(f'{save_path}/images_raw.jpg')
     else:
-        fig.suptitle(f'Epoch {epoch_num}', fontsize=48)
-        plt.savefig(f"{save_path}/images_patch_drop_{epoch_num}.jpg")
+        suptitle_str = f'Epoch: {epoch_num+1}, validation accuracy: {args.epoch_acc:.4f}, ' \
+                       f'using {"perturbed top-k" if args.topk_selection else "gumbel softmax"} predictor\n' \
+                       f'Pruning patches before layers [{",".join(str(loc) for loc in args.pruning_locs)}], ' \
+                       f'with keeping ratios of [{",".join(str(round(ratio, 2)) for ratio in args.keep_ratios)}] '
+        if args.topk_selection:
+            suptitle_str += f'current sigma: {args.current_sigma:.4f}'
+        fig.suptitle(suptitle_str, fontsize=48)
+        # f'{"applying attention masking" if not args.zero_drop else "setting discarded patches to zero"}',
+        # f'Selecting patches based on {"predictor scores" if not args.attn_selection else "CLS token attention weights"}, '
+        plt.savefig(f"{save_path}/images_patch_drop_{epoch_num+1}.jpg")
 
-#def display_patch_drop(images, keep_decisions, save_path, epoch_num):
-#    B, _, H, W = images.shape
-#    pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
-#    if epoch_num==0:
-#        # default grid size is 8 images per row
-#        # save unmodified images in first epoch
-#        vutils.save_image(vutils.make_grid(images, normalize=False, scale_each=True),
-#                          f"{save_path}/image.jpg")
-#
-#    patch_mask = generate_patch_mask(H, keep_decisions)
-#
-#    images = images * patch_mask
-#    # default grid size is 8 images per row
-#    vutils.save_image(vutils.make_grid(images, normalize=False, scale_each=True),
-#                      f"{save_path}/image_{epoch_num}.jpg")
+    plt.close()
+
 
 def get_jaccard_similarity(patch_drop_mask, threshold_attention_mask):
     B, C, H, W = patch_drop_mask.shape
