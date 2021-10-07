666162be9616140a32361412c15e2afda1a520c9
Marc Seger <m.seger345@gmail.com>
2021-10-07 14:25:31 +0200

    update of visualization function calls

--- mask_predictor.py
+++ mask_predictor.py
@@ -210,26 +210,14 @@ def train_model(args, model, criterion, optimizer, mask_test_imgs, mask_test_lab
 
         with torch.no_grad():
             model.eval()
-            test_logits, _, final_cls_attn, final_policy = model(mask_test_imgs.clone())
-            final_cls_attn = final_cls_attn.detach()
+            if args.early_exit:
+                test_logits, _, cls_attns, final_policy = model(mask_test_imgs.clone())
+            else:
+                test_logits, cls_attns, final_policy = model(mask_test_imgs.clone())
             test_preds = torch.argmax(test_logits, dim=1)
 
-            patch_keep_prob = torch.exp(getattr(model, 'current_score')[:, :, 0])
-
-            # list that holds the original patch indices (based on initial, unpruned tokens sequence) sorted in
-            # descending order based on the prediction scores of the respective prediction module (length of list is
-            # equal to the number of prediction modules)
-            decisions = getattr(model, "sorted_patch_indices")
-            final_decision = decisions[-1]
-            num_final_kept_tokens = int(decisions[0].shape[1] * model.token_ratio[-1])
-            # indices of kept tokens after the final predictor module
-            kept_token_idx = final_decision[:, :num_final_kept_tokens]
-            dropped_token_idx = []
-            for i, decision in enumerate(decisions):
-                num_kept_tokens = int(decisions[0].shape[1] * model.token_ratio[i])
-                dropped_token_idx.append(decision[:, num_kept_tokens:])
-            # all the token indices corresponding to the dropped patches
-            dropped_token_idx = torch.cat(dropped_token_idx, dim=1)
+            kept_token_idx = getattr(model, "kept_token_indices")
+            dropped_token_idx = getattr(model, "dropped_token_indices")
             token_idx = torch.cat((kept_token_idx, dropped_token_idx), dim=1)
 
             keep_mask = torch.ones_like(kept_token_idx)
@@ -237,14 +225,35 @@ def train_model(args, model, criterion, optimizer, mask_test_imgs, mask_test_lab
             sorted_patch_drop_mask = torch.cat((keep_mask, drop_mask), dim=1)
             patch_drop_mask = torch.empty_like(sorted_patch_drop_mask)
             patch_drop_mask.scatter_(dim=1, index=token_idx.long(), src=sorted_patch_drop_mask).unsqueeze(-1)
+            if not args.topk_selection:
+                patch_keep_prob = torch.exp(getattr(model, 'current_score')[:, :, 0])
             # only display result after last predictor stage
             attention_segmentation.display_patch_drop(mask_test_imgs.cpu(), patch_drop_mask.cpu(), args, epoch,
                                                       (test_preds == mask_test_labels).cpu().numpy(),
                                                       patch_indices=[kept_token_idx.cpu(), dropped_token_idx.cpu()],
-                                                      patch_scores=patch_keep_prob.cpu())
-            # attention_segmentation.visualize_heads(mask_test_imgs.cpu(), args, epoch,
-            #                                        [kept_token_idx.cpu(), dropped_token_idx.cpu()],
-            #                                        final_cls_attn=final_cls_attn.cpu())
+                                                      patch_scores=patch_keep_prob.cpu() if not args.topk_selection
+                                                      else None)
+
+            padded_cls_attns = []
+            for i, attn in enumerate(cls_attns):
+                if i < args.pruning_locs[0]:
+                    B, H, N = attn[:, :, 1:].shape
+                    padded_cls_attns.append(attn.unsqueeze(1))
+                else:
+                    B, H, N_kept = attn[:, :, 1:].shape
+                    padded_attn = torch.cat((attn, torch.zeros((B, H, N-N_kept),
+                                                               device=attn.device, dtype=attn.dtype)), dim=2)
+                    padded_cls_attns.append(padded_attn.unsqueeze(1))
+
+            # concatenate the list of class attentions after each encoder layer
+            # permute layer and batch dimension, such that we can visualize the evolution of the CLS token for the same
+            # image across all layers in one picture and loop over the batch dimension to plot this picture for every
+            # input image in the batch
+            cls_attns = torch.cat(padded_cls_attns, dim=1)  # (B, L, H, N+1)
+            for b in range(cls_attns.shape[0]):
+                attention_segmentation.visualize_heads(mask_test_imgs[b].cpu(), args, epoch + 1,
+                                                       [kept_token_idx.cpu(), dropped_token_idx.cpu()],
+                                                       cls_attns[b].cpu(), b)
 
     time_elapsed = time.time() - since
     print(f'Training complete in {(time_elapsed // 60):.0f}m {(time_elapsed % 60):.0f}s')
