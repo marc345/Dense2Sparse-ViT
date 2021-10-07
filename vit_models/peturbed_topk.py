7438413713717f999e502651cacbff85c488c6f9
Marc Seger <m.seger345@gmail.com>
2021-10-07 14:18:18 +0200

    cuda memory profiling of forward pass in perturbed top k module

--- vit_models/peturbed_topk.py
+++ vit_models/peturbed_topk.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class PerturbedTopK(nn.Module):
-    def __init__(self, k: int, num_samples: int = 1000, sigma: float = 0.05):
+    def __init__(self, k: int, num_samples: int = 500, sigma: float = 0.05):
         super(PerturbedTopK, self).__init__()
         self.num_samples = num_samples
         self.sigma = sigma
@@ -15,7 +15,15 @@ class PerturbedTopK(nn.Module):
 
 class PerturbedTopKFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x, k: int, num_samples: int = 1000, sigma: float = 0.05):
+    def forward(ctx, x, k: int, num_samples: int = 500, sigma: float = 0.05):
+        # t = torch.cuda.get_device_properties(0).total_memory
+        # r = torch.cuda.memory_reserved(0)
+        # a = torch.cuda.memory_allocated(0)
+        # f = r - a  # free inside reserved
+        #
+        # print(f'Before Top-K: total: {t/1e9:2.2f}, reserved: {r/1e9:2.2f}, '
+        #       f'allocated: {a/1e9:2.2f}, free: {f/1e9:2.2f}')
+
         b, d = x.shape
         # for Gaussian: noise and gradient are the same.
         noise = torch.normal(mean=0.0, std=1.0, size=(b, num_samples, d)).to(x.device)
@@ -25,9 +33,29 @@ class PerturbedTopKFunction(torch.autograd.Function):
         indices = topk_results.indices  # b, nS, k
         indices = torch.sort(indices, dim=-1).values  # b, nS, k
 
+        # f_new = torch.cuda.memory_reserved(0) - torch.cuda.memory_allocated(0)
+        # perturbation_use = f - f_new
+        # print(f'USED BY TOP-K PERTURBATIONS [GB]: {perturbation_use/1e9:2.2f}, '
+        #       f'now left free [GB]: {f_new/1e9:2.2f}')
+        # f = f_new
+
         # b, nS, k, d
         perturbed_output = torch.nn.functional.one_hot(indices, num_classes=d).float()
+
+        # f_new = torch.cuda.memory_reserved(0) - torch.cuda.memory_allocated(0)
+        # one_hot_use = f - f_new
+        # print(f'USED BY TOP-K ONE-HOT PERTURBATIONS [GB]: {one_hot_use/1e9:2.2f}, '
+        #       f'now left free [GB]: {f_new/1e9:2.2f}')
+        # f = f_new
+
         indicators = perturbed_output.mean(dim=1)  # b, k, d
+
+        # f_new = torch.cuda.memory_reserved(0) - torch.cuda.memory_allocated(0)
+        # indicators_use = f - f_new
+        # print(f'USED BY TOP-K INDICATORS [GB]: {indicators_use/1e9:2.2f}, '
+        #       f'now left free [GB]: {f_new/1e9:2.2f}')
+        # f = f_new
+        # print(f'TOTAL TOP-K MEMORY USAGE [GB]: {(perturbation_use+one_hot_use+indicators_use)/1e9:2.2f}')
 
         # constants for backward
         ctx.k = k
