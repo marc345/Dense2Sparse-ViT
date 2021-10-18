ca43cf4abf0905e01af3cda3b8b40697e8448806
Marc Seger <m.seger345@gmail.com>
2021-10-18 17:23:54 +0200

    added function to return CLS attentions from teacher network

--- vit_models/dynamic_vit.py
+++ vit_models/dynamic_vit.py
@@ -763,6 +763,23 @@ class VisionTransformerTeacher(nn.Module):
         self.num_classes = num_classes
         self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
+    def forward_cls_attention(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        cls_attn_weights = []
+        for i, blk in enumerate(self.blocks):
+            x, cls_attns = blk(x, return_cls_attn=True)
+            cls_attn_weights.append(cls_attns.detach())
+
+        return torch.stack(cls_attn_weights, dim=1)
+
+
     def forward(self, x, return_final_cls_attn=False):
         B = x.shape[0]
         x = self.patch_embed(x)
