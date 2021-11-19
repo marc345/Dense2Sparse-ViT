32066900d08cd3eb5ff8a89ebc19119d5345dd5c
Marc Seger <m.seger345@gmail.com>
2021-11-19 12:48:25 +0100

    added normalization of validation data :)

--- mask_predictor.py
+++ mask_predictor.py
@@ -58,6 +58,8 @@ data_transforms = {
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
 
